374792198f0d6809bff0bf3e361719b8984ad48b
Stas Merzlyakov <st.merzlyakov@yandex.ru>
2020-01-20 18:11:34 +0500

    fix comment

--- install/001_redmine.sh
+++ install/001_redmine.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 ## !!! sudo required
-#
-## install apache2
-#apt -y install apache2 apache2-dev libcurl4-openssl-dev
-#apt -y install imagemagick libmagickwand-dev git build-essential automake libgmp-dev
-#
-## install postgresql
-#echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
-#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-#apt update
-#apt -y install postgresql
-#systemctl start postgresql
-#systemctl enable postgresql
-#
-#su - postgres -c 'createuser redmine'
-#su - postgres -c 'echo "ALTER USER redmine WITH ENCRYPTED password '\''redmine'\'';" | psql'
-#su - postgres -c 'echo "CREATE DATABASE redmine WITH ENCODING='\''UTF8'\'' OWNER=redmine;" | psql'
-#apt -y install libpqxx-dev protobuf-compiler
-#
-## ruby
-#apt-add-repository -y ppa:rael-gc/rvm
-#apt update
-#apt -y install rvm
-#su -c 'echo "source /etc/profile.d/rvm.sh" | tee -a /etc/profile'
-#su -c 'source /etc/profile.d/rvm.sh && rvm install 2.6.5 && rvm use 2.6.5 --default && gem install bundler'
-#
-#su -c 'source /etc/profile.d/rvm.sh && gem install passenger'
-#su -c 'source /etc/profile.d/rvm.sh && passenger-install-apache2-module -a'
-#
+
+# install apache2
+apt -y install apache2 apache2-dev libcurl4-openssl-dev
+apt -y install imagemagick libmagickwand-dev git build-essential automake libgmp-dev
+
+# install postgresql
+echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt update
+apt -y install postgresql
+systemctl start postgresql
+systemctl enable postgresql
+
+su - postgres -c 'createuser redmine'
+su - postgres -c 'echo "ALTER USER redmine WITH ENCRYPTED password '\''redmine'\'';" | psql'
+su - postgres -c 'echo "CREATE DATABASE redmine WITH ENCODING='\''UTF8'\'' OWNER=redmine;" | psql'
+apt -y install libpqxx-dev protobuf-compiler
+
+# ruby
+apt-add-repository -y ppa:rael-gc/rvm
+apt update
+apt -y install rvm
+su -c 'echo "source /etc/profile.d/rvm.sh" | tee -a /etc/profile'
+su -c 'source /etc/profile.d/rvm.sh && rvm install 2.6.5 && rvm use 2.6.5 --default && gem install bundler'
+
+su -c 'source /etc/profile.d/rvm.sh && gem install passenger'
+su -c 'source /etc/profile.d/rvm.sh && passenger-install-apache2-module -a'
+
 ## install redmine
 #adduser --disabled-password --gecos "Redmine User" redmine
 su - redmine -c 'cd ~
