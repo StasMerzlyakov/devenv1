5bc057be2afa9ac10576f98b8e988ac69ee7667a
Stas Merzlyakov <st.merzlyakov@yandex.ru>
2020-05-19 17:27:37 +0400

    новая ветка

---
+++ install/002_nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[ ! -f ./variables ] && echo "file ./variables not exists" && exit 1
+
+source ./variables
+
+yum -y install 
+
+echo "[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/
+gpgcheck=0
+enabled=1" > /etc/yum.repos.d/nginx.repo
+
+yum -y install nginx
+systemctl enable nginx
