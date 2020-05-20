0675aaf6d8e3385942f25ab23b7ccd28af566366
Stas Merzlyakov <st.merzlyakov@yandex.ru>
2020-05-20 17:43:44 +0400

    nginx sample

--- install/002_nginx.sh
+++ install/002_nginx.sh
@@ -7,13 +7,33 @@ basearch=$(rpm -q --qf "%{arch}" -f /etc/$distro)
 
 echo "[nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
 gpgcheck=0
 enabled=1" > /etc/yum.repos.d/nginx.repo
 
 yum -y install nginx
 systemctl enable nginx
 
-
-
-
+# sample
+# echo "upstream authproxy {
+#     server localhost:8080;
+# }
+# 
+# server {
+#     listen 9090 default;
+#     location / {
+#         proxy_pass http://authproxy;
+#         proxy_http_version 1.1;
+#         proxy_set_header Upgrade $http_upgrade;
+#         proxy_set_header Connection "upgrade";
+#     }
+# }" > /etc/nginx/conf.d/authproxy.conf
+# 
+# systemctl start nginx
+# 
+# 
+# # fix SELinux
+# setsebool -P httpd_can_network_connect 1
+# 
+# 
+# 
